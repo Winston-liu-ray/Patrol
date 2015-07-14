f7f9fd8a09490767b6973b338400d6b4ca36ac14
Ｗinston <winston@ip134-116.wlan.ntust.edu.tw>
2015-07-14 13:41:49 +0800

    Initial Commit

---
+++ Patrol/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Patrol
+//
+//  Created by Ｗinston on 7/14/15.
+//  Copyright (c) 2015 Ｗinston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Patrol/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Patrol
+//
+//  Created by Ｗinston on 7/14/15.
+//  Copyright (c) 2015 Ｗinston. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
